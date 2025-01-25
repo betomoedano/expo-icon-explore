454b7005a90cb6488b92b2d82f28bae934c2b174
evanbacon <bacon@expo.io>
2025-01-25 13:38:25 -0800

    publish

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,14 @@
-import "../global.css";
 import { Slot } from "expo-router";
+import Head from "expo-router/head";
+import "../global.css";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <>
+      <Head>
+        <title>Expo icon search</title>
+      </Head>
+      <Slot />
+    </>
+  );
 }
